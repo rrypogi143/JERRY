8629abf47721b5ea61b103a5a669c4a821b56ab3
rrypogi143 <rryyibanez@gmail.com>
2025-04-14 16:58:48 +0800

    Add files via upload

---
+++ hci3.js
@@ -0,0 +1,6 @@
+function showFact() {
+    const textElement = document.getElementById("dynamic-text");
+    textElement.textContent = "IT stands for Information Technology. It's a vital part of our digital world!";
+    textElement.classList.remove("hidden");
+  }
+  
